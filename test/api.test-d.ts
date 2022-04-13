b50ce7f552c51965401ea1abe0809d5e5b17c8de
Andreas Pizsa <hopster@gmail.com>
2022-04-13 07:10:40 +0200

    chore: complete package.json (homepage, repository, bugs)
    Signed-off-by: Andreas Pizsa <hopster@gmail.com>

--- test/api.test-d.ts
+++ test/api.test-d.ts
@@ -1,7 +1,8 @@
 import {expectType, expectError} from 'tsd'
 import createFaviconKitApi, {UrlString} from '..'
 
-const api = createFaviconKitApi()
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const api = createFaviconKitApi({})
 expectType<UrlString>(api.iconUrl('example.com', 64))
 expectType<UrlString>(api.iconUrl('http://example.com', 64))
 expectType<UrlString>(api.iconUrl('https://example.com', 64))
